c9866a3141601b189fa456c24c92425c9e03db95
Varinder Singh <Varinder.Singh@timesinternet.in>
2018-06-20 08:48:42 +0530

    animations

--- src/aboutme/aboutme.js
+++ src/aboutme/aboutme.js
@@ -3,37 +3,69 @@ import ReactDOM from "react-dom";
 import styles from "../aboutme/aboutme.css";
 import $ from "jquery";
 
-
-
-
-
 class Aboutme extends Component {
   constructor(props) {
     super(props);
   }
   componentDidMount() {
     const el = ReactDOM.findDOMNode(this.refs.toggle);
+    const em = ReactDOM.findDOMNode(this.refs.text);
     $(el).slideDown();
+
+  
+  // text expand animation
+    $(function() {
+      var text = $(em);
+      $(window).scroll(function() {
+        var scroll = $(window).scrollTop();
+
+        if (scroll >= 200) {
+          text.removeClass("hidden");
+        } else {
+          text.addClass("hidden");
+        }
+      });
+    });
+  
+  
+  
   }
   render() {
     return (
       <div className="aboutme">
-        {/* <h1>
-                    Front-end Web Developer.
-                </h1> */}
-       
+        {/* <h1>Front-end Web Developer.</h1> */}
+
         <p ref="toggle" className="toggle">
-          HI,<br /> I am Halifax, NS.<br /> Specializing in Website development.
-          If you are a business seeking a web presence or an employer looking to
-          hire, you can get in touch with me here.
+          
+            <div className="mb14"> HI,</div>
+            <ul className="text hidden" ref="text">
+              <li>I</li>
+              <li className="spaced">A</li>
+              <li className="ghost">M</li>
+              <li className="spaced">D</li>
+              <li className="ghost">e</li>
+              <li className="ghost">s</li>
+              <li className="ghost">i</li>
+              <li className="ghost">g</li>
+              <li className="ghost">n</li>
+              <li className="spaced">N</li>
+              <li className="ghost">e</li>
+              <li className="ghost">t</li>
+              <li className="ghost">w</li>
+              <li className="ghost">o</li>
+              <li className="ghost">r</li>
+              <li className="ghost">k</li>
+            </ul>
+            <div>asd asd asdf asdf asdfasd asdas asd sd.</div>
+          
         </p>
         <p className="buttons_cont">
-          {" "}
-          <a href="/contact" class="button_link red mr10" >
-            I need a Website
+          
+          <a href="/contact" class="button_link red mr10">
+            aaaaaaaaaaaaa
           </a>
           <a class="button_link grey ml10" href="#">
-            I am looking to Hire
+            bbbbbbbb
           </a>
         </p>
       </div>
--- src/contactform/contactform.js
+++ src/contactform/contactform.js
@@ -1,22 +1,43 @@
-import React, { Component } from 'react';
-import styles from '../contactform/contactform.css';
+import React, { Component } from "react";
+import styles from "../contactform/contactform.css";
 
 class ContactForm extends Component {
-    render() {
-        return (
-            <div class="contactform_cont">
-<div className="contact_txt">Contact Me</div>
-<form>
-<input className="name mr10" name="name" type="text"  placeholder="Name"/>
-<input className="email ml10" name="email" type="text"  placeholder="Email"/><br/>
-<input className="subject" name="subject"  type="text" placeholder="Subject"/><br/>
-<textarea name="message" className="message" placeholder="Message"/>
-<br/><br/>
-<input type="submit" class="btn_small"/>
-</form>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div class="contactform_cont">
+        <div className="contact_txt">Contact Me</div>
+        <form>
+          <input
+            autocomplete="off"
+            className="name mr10"
+            name="name"
+            type="text"
+            placeholder="Name"
+          />
+          <input
+            autocomplete="off"
+            className="email ml10"
+            name="email"
+            type="text"
+            placeholder="Email"
+          />
+          <br />
+          <input
+            autocomplete="off"
+            className="subject"
+            name="subject"
+            type="text"
+            placeholder="Subject"
+          />
+          <br />
+          <textarea name="message" className="message" placeholder="Message" />
+          <br />
+          <br />
+          <input type="submit" class="btn_small" />
+        </form>
+      </div>
+    );
+  }
 }
 
 export default ContactForm;
